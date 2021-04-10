8e7cefe3d3d35d2b73c8bbe4e4eb7950e86c2d99
Louisp78 <louis.place@epita.fr>
2021-04-10 14:40:54 +0200

    :wrench: Test php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/main.css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+    <script src='assets/js/main.js'></script>
+</head>
+<body>
+    <h1>Un peu de patience, ce site se construit ...</h1>
+</body>
+</html>
