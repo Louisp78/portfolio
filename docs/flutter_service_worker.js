986cd7ffdba1ff2de849c37b3926bce3f1e7777e
Louis Place <louis.place@epita.fr>
2023-08-02 03:21:51 +0200

    fix 5

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -29,8 +29,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "99365898ddc0391738aa8c237e435bfb
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8c3c48025363871bdb229ed219b42436",
-"/": "8c3c48025363871bdb229ed219b42436",
+"index.html": "08f596f330595a414c5a42cbccf3fd23",
+"/": "08f596f330595a414c5a42cbccf3fd23",
 "main.dart.js": "2b7d80ff7b8418dec4405fa769601891",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
