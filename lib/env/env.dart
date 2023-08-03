45122d85a8261d54de995a2f3aacedb3b253de3b
Louis Place <louis.place@epita.fr>
2023-08-04 01:56:50 +0200

    try to use printf inside the workflow

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -6,16 +6,22 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'FIREBASE_API', obfuscate: true)
   static final String firebaseApi = _Env.firebaseApi;
+
   @EnviedField(varName: 'FIREBASE_AUTH_DOMAIN', obfuscate: true)
   static final String firebaseAuthDomain = _Env.firebaseAuthDomain;
+
   @EnviedField(varName: 'FIREBASE_PROJECT_ID', obfuscate: true)
   static final String firebaseProjectId = _Env.firebaseProjectId;
+
   @EnviedField(varName: 'FIREBASE_STORAGE_BUCKET', obfuscate: true)
   static final String firebaseStorageBucket = _Env.firebaseStorageBucket;
+
   @EnviedField(varName: 'FIREBASE_MESSAGING_SENDER_ID', obfuscate: true)
   static final String firebaseMessagingSenderId = _Env.firebaseMessagingSenderId;
+
   @EnviedField(varName: 'FIREBASE_APP_ID', obfuscate: true)
   static final String firebaseAppId = _Env.firebaseAppId;
+
   @EnviedField(varName: 'FIREBASE_MEASUREMENT_ID', obfuscate: true)
   static final String firebaseMeasurementId = _Env.firebaseMeasurementId;
 }
