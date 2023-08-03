8b9b79c0cbca9f3f111b65d1d4245d46072202e2
Louis Place <louis.place@epita.fr>
2023-08-04 01:48:18 +0200

    try to make env.dart var final

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -5,18 +5,17 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'FIREBASE_API', obfuscate: true)
-  static String firebaseApi = _Env.firebaseApi;
+  static final String firebaseApi = _Env.firebaseApi;
   @EnviedField(varName: 'FIREBASE_AUTH_DOMAIN', obfuscate: true)
-  static String firebaseAuthDomain = _Env.firebaseAuthDomain;
+  static final String firebaseAuthDomain = _Env.firebaseAuthDomain;
   @EnviedField(varName: 'FIREBASE_PROJECT_ID', obfuscate: true)
-  static String firebaseProjectId = _Env.firebaseProjectId;
+  static final String firebaseProjectId = _Env.firebaseProjectId;
   @EnviedField(varName: 'FIREBASE_STORAGE_BUCKET', obfuscate: true)
-  static String firebaseStorageBucket = _Env.firebaseStorageBucket;
+  static final String firebaseStorageBucket = _Env.firebaseStorageBucket;
   @EnviedField(varName: 'FIREBASE_MESSAGING_SENDER_ID', obfuscate: true)
-  static  String firebaseMessagingSenderId =
-      _Env.firebaseMessagingSenderId;
+  static final String firebaseMessagingSenderId = _Env.firebaseMessagingSenderId;
   @EnviedField(varName: 'FIREBASE_APP_ID', obfuscate: true)
-  static String firebaseAppId = _Env.firebaseAppId;
+  static final String firebaseAppId = _Env.firebaseAppId;
   @EnviedField(varName: 'FIREBASE_MEASUREMENT_ID', obfuscate: true)
-  static String firebaseMeasurementId = _Env.firebaseMeasurementId;
+  static final String firebaseMeasurementId = _Env.firebaseMeasurementId;
 }
