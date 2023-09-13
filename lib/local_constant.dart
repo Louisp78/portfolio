c6ce7cad30ecd0cc6d364173ff1ff8a52b524979
Louis Place <louis.place@epita.fr>
2023-09-13 20:15:57 +0200

    a better profile photo

--- lib/local_constant.dart
+++ lib/local_constant.dart
@@ -4,5 +4,5 @@ Vous cherchez à réaliser une application web ou mobile ? Je suis prêt à rele
 Freelance passionné par la création d'application web et mobile qui répondent à vos besoins,
 engagez-moi dès maintenant pour réaliser votre projet !
   """;
-  static const String profileImgPath = 'lib/assets/images/profile2.jpg';
+  static const String profileImgPath = 'lib/assets/images/profile2.png';
 }
